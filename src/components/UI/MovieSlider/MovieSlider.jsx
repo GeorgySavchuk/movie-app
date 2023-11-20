1f3a6324a57092febed45015cfe45a99a7c510ad
GeorgySavchuk <savchukg89@gmail.com>
2023-11-20 23:55:59 +0300

    Исправил кнопки в слайдере

--- src/components/UI/MovieSlider/MovieSlider.jsx
+++ src/components/UI/MovieSlider/MovieSlider.jsx
@@ -16,28 +16,30 @@ const MovieSlider = ({movies, setImageProps, isPropsFromMoviePage}) => {
     };
     return (
         <>
-            <div className={classes.similarMoviesLeftArrowContainer} onClick={handlePrevClick} ref={leftArrow}>
-                <FaChevronLeft className={classes.similarMoviesLeftArrow}/>
-            </div>
-            <div className={classes.similarMoviesSlider} ref={slider}>
-                {movies.map(movie =>
-                    <div className={classes.similarMovie} ref={newMovie}>
-                        <MovieCard name={movie.name} src={movie.poster.url} rating={movie.rating} type={movie.type}
-                                   year={movie.year} movieLength={movie.movieLength}
-                                   key={`${movie.name}:${movie.id}:${movie.year}`} width={'100%'} height={'100%'} id={movie.id}
-                                   alt={movie.name} backdrop={movie.backdrop} genres={movie.genres}
-                                   shortDescription={movie.shortDescription} countries={movie.countries}
-                                   budget={movie.budget} fees={movie.fees} similarMovies={movie.similarMovies}
-                                   ageRating={movie.ageRating} videos={movie.videos}
-                                   description={movie.description} persons={movie.persons} slogan={movie.slogan}
-                                   seasonsInfo={movie.seasonsInfo} premiere={movie.premiere} logo={movie.logo}
-                                   setImageProps={setImageProps} isPropsFromMoviePage={isPropsFromMoviePage}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className={classes.similarMoviesRightArrowContainer} onClick={handleNextClick} ref={rightArrow}>
-                <FaChevronRight className={classes.similarMoviesRightArrow}/>
+            <div style={{position: 'relative', width: '100%', height: '100%'}}>
+                <div className={classes.similarMoviesLeftArrowContainer} onClick={handlePrevClick} ref={leftArrow}>
+                    <FaChevronLeft className={classes.similarMoviesLeftArrow}/>
+                </div>
+                <div className={classes.similarMoviesSlider} ref={slider}>
+                    {movies.map(movie =>
+                        <div className={classes.similarMovie} ref={newMovie}>
+                            <MovieCard name={movie.name} src={movie.poster.url} rating={movie.rating} type={movie.type}
+                                       year={movie.year} movieLength={movie.movieLength}
+                                       key={`${movie.name}:${movie.id}:${movie.year}`} width={'100%'} height={'100%'} id={movie.id}
+                                       alt={movie.name} backdrop={movie.backdrop} genres={movie.genres}
+                                       shortDescription={movie.shortDescription} countries={movie.countries}
+                                       budget={movie.budget} fees={movie.fees} similarMovies={movie.similarMovies}
+                                       ageRating={movie.ageRating} videos={movie.videos}
+                                       description={movie.description} persons={movie.persons} slogan={movie.slogan}
+                                       seasonsInfo={movie.seasonsInfo} premiere={movie.premiere} logo={movie.logo}
+                                       setImageProps={setImageProps} isPropsFromMoviePage={isPropsFromMoviePage}
+                            />
+                        </div>
+                    )}
+                </div>
+                <div className={classes.similarMoviesRightArrowContainer} onClick={handleNextClick} ref={rightArrow}>
+                    <FaChevronRight className={classes.similarMoviesRightArrow}/>
+                </div>
             </div>
         </>
     );
