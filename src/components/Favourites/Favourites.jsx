28281cc4c753571c0bd59810422f1646a340db05
GeorgySavchuk <savchukg89@gmail.com>
2023-11-27 10:06:09 +0300

    Теперь при добавлении элемента в избранное он сохраняется в localStorage

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -3,28 +3,30 @@ import {useFavourites} from "../../hooks/useFavourites";
 import classes from './Favourites.module.css'
 import MovieCard from "../UI/MovieCard/MovieCard";
 const Favourites = () => {
-    const {favourites} = useFavourites()
+    const {favouriteMovies} = useFavourites()
     useEffect(() => {
         document.title = `Мир Кино | Избранное`
     }, []);
     return (
         <div className={classes.container}>
             <h1 style={{marginBottom: '20px', fontStyle: '30px'}}>Избранное</h1>
-            {favourites.length === 0 && <div style={{textAlign: 'center'}}>
+            {!favouriteMovies || favouriteMovies.length === 0 && <div style={{textAlign: 'center'}}>
                 <span className={classes.empty}>Список избранного пуст</span>
             </div>}
             <div className={classes.favourites}>
-                {favourites.map(favouriteMovie =>
-                    <MovieCard key={`${favouriteMovie.name}:${favouriteMovie.id}:${favouriteMovie.year}`} id={favouriteMovie.id} rating={favouriteMovie.rating}
-                           name={favouriteMovie.name} year={favouriteMovie.year}
-                           movieLength={favouriteMovie.movieLength} type={favouriteMovie.type} alt={favouriteMovie.name}
-                           height={'100%'} width={'100%'} src={favouriteMovie.src}
-                           backdrop={favouriteMovie.backdrop} genres={favouriteMovie.genres} shortDescription={favouriteMovie.shortDescription} countries={favouriteMovie.countries}
-                           budget={favouriteMovie.budget} fees={favouriteMovie.fees} similarMovies={favouriteMovie.similarMovies} ageRating={favouriteMovie.ageRating} videos={favouriteMovie.videos}
-                           description={favouriteMovie.description} persons={favouriteMovie.persons} slogan={favouriteMovie.slogan} seasonsInfo={favouriteMovie.seasonsInfo} logo={favouriteMovie.logo}
-                           premiere={favouriteMovie.premiere} isPropsFromMoviePage={false}
-                    />
-                )}
+                {
+                    favouriteMovies && favouriteMovies.map(favouriteMovie =>
+                        <MovieCard key={`${favouriteMovie.name}:${favouriteMovie.id}:${favouriteMovie.year}`} id={favouriteMovie.id} rating={favouriteMovie.rating}
+                                   name={favouriteMovie.name} year={favouriteMovie.year}
+                                   movieLength={favouriteMovie.movieLength} type={favouriteMovie.type} alt={favouriteMovie.name}
+                                   height={'100%'} width={'100%'} src={favouriteMovie.src}
+                                   backdrop={favouriteMovie.backdrop} genres={favouriteMovie.genres} shortDescription={favouriteMovie.shortDescription} countries={favouriteMovie.countries}
+                                   budget={favouriteMovie.budget} fees={favouriteMovie.fees} similarMovies={favouriteMovie.similarMovies} ageRating={favouriteMovie.ageRating} videos={favouriteMovie.videos}
+                                   description={favouriteMovie.description} persons={favouriteMovie.persons} slogan={favouriteMovie.slogan} seasonsInfo={favouriteMovie.seasonsInfo} logo={favouriteMovie.logo}
+                                   premiere={favouriteMovie.premiere} isPropsFromMoviePage={false}
+                        />
+                    )
+                }
             </div>
         </div>
     );
--- src/hooks/useFavourites.js
+++ src/hooks/useFavourites.js
@@ -7,18 +7,40 @@ export function useFavourites() {
 }
 
 export function FavouritesProvider({ children }) {
-    const [favourites, setFavourites] = useState([]);
+    const [favouriteMovies, setFavouriteMovies] = useState(JSON.parse(localStorage.getItem('favourites'))?.favourites)
     const toggleFavourite = (props) => {
-        if (favourites.some((favouriteMovie) => favouriteMovie.name === props.name)) {
-            setFavourites(favourites.filter((favouriteMovie) => favouriteMovie.name !== props.name));
+        if(favouriteMovies) {
+            if (favouriteMovies.some((favouriteMovie) => favouriteMovie.name === props.name)) {
+                const obj = JSON.parse(localStorage.getItem('favourites'))
+                obj.favourites = favouriteMovies.filter((favouriteMovie) => favouriteMovie.name !== props.name)
+                setFavouriteMovies(favouriteMovies.filter((favouriteMovie) => favouriteMovie.name !== props.name));
+                localStorage.setItem('favourites', JSON.stringify(obj))
+
+            } else {
+                const obj = JSON.parse(localStorage.getItem('favourites'));
+                obj.favourites.push(props);
+                localStorage.setItem('favourites', JSON.stringify(obj));
+                setFavouriteMovies([...favouriteMovies, props]);
+            }
         } else {
-            setFavourites([...favourites, props]);
+            const favourites = [];
+            favourites.push(props);
+            const obj = {
+                favourites: favourites
+            }
+            localStorage.setItem('favourites', JSON.stringify(obj));
+            setFavouriteMovies([props])
         }
     };
-    const isFavourite = (props) => favourites.some(favouriteMovie => favouriteMovie.name === props.name)
+    const isFavourite = props => {
+        if(favouriteMovies) {
+            return favouriteMovies.some((favouriteMovie) => favouriteMovie.name === props.name)
+        }
+        return false
+    }
 
     return (
-        <FavoritesContext.Provider value={{favourites, isFavourite, toggleFavourite }}>
+        <FavoritesContext.Provider value={{favouriteMovies, isFavourite, toggleFavourite }}>
             {children}
         </FavoritesContext.Provider>
     );
